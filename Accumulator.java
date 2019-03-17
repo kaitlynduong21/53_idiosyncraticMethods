93af2939fdd66e41a881cdc8a2c6f2204de24f6f
kduong@stuy.edu <kduong@stuy.edu>
2019-03-17 12:10:55 -0400

    ficed Accumulator

--- Accumulator.java
+++ Accumulator.java
@@ -1,111 +1,3 @@
-/*
-Java protects a programmer against applying a method to elements in list when some elements of the list might omit support for a particular operation.
-
-This protection is implemented by the compiler. The following attempt to violate the restriction is expected to result in the error "cannot find symbol".
-
-public class Test {
-
-	public static void main (String[] args) {
-
-		Object[] list =  new Object[5];
-
-		list[0] = 1;
-		list[1] = 3.14;
-		list[2] = "hi";
-		list[3] = "there";
-		list[4] = 420;
-
-		for (Object element : list) {
-
-			if (element.startsWith("h"))
-			System.out.println(element);
-		}
-	}
-}
-*/
-
-/*
-A programmer should expect to be able to work around the stumbling block because the JVM knows the type of an element.
-
-Java's new operator identifies the type of an element to the JVM
-*/
-
-/*
-Use of the operator alone is insufficient because the compiler continues to stumble over the previous stumbling block of protection against mis-applying a method.
-
-public class Test {
-
-	public static void main (String[] args) {
-
-		Object[] list =  new Object[1];
-
-		list[0] = new String("hi");
-
-		for (Object element : list) {
-
-			if (element.startsWith("h"))
-			System.out.println(element);
-		}
-	}
-}
-
-predicted error message: cannot find symbol
-
-cannot find symbol
-*/
-
-/*
-Java provides a casting facility that a programmer uses to tell the compiler that they are using a subclass's method on an object for which a reference is stored in a super-type variable.
-
-public class Test {
-
-	public static void main (String[] args) {
-
-		Object[] list =  new Object[2];
-
-		list[0] = new String("hi");
-    list[1] = 1;
-
-		for (Object element : list) {
-
-			if (((String) element).startsWith("h"))
-			System.out.println(element);
-		}
-	}
-}
-
-
-But use of that facility alone is insufficient because the JVM stumbles over not being able to cast certain subclasses of Object into other subclasses.
-For example, it can't convert Integer to String, so a runtime error occurs.
-
-predicted error message: cannot be cast
-
-java.base/java.lang.Integer cannot be cast to java.base/java.lang.String
-*/
-
-/*
-A programmer can combine use of the operator and the facility to apply a method to exactly those elements in the list that support the method.
-
-public class Test {
-
-	public static void main (String[] args) {
-
-		Object[] list =  new Object[2];
-
-		list[0] = new String("hi");
-    list[1] = 1;
-
-		for (Object element : list) {
-
-      if(element instanceof String) {
-			   if (((String) element).startsWith("h"))
-			      System.out.println(element);
-		  }
-	  }
-  }
-}
-*/
-
 public class Accumulator {
 
   public static String catElementsStartingWith (List_inArraySlots list, String pre) {
@@ -140,3 +32,144 @@ public class Accumulator {
 		return finites;
   }
 }
+
+/* 3.  Stumbling block 0
+              Java protects a programmer against applying a method to
+              elements in list when some elements of the list might
+              omit support for a particular operation.
+              This protection is implemented by the compiler
+              The following code violates the restriction:
+             */
+           // [code that violates the restriction]
+           /*
+            Object[] list =  new Object[5];
+
+       		   list[0] = 1;
+       		   list[1] = 3.14;
+       		   list[2] = "hi";
+       		   list[3] = "there";
+       		   list[4] = 420;
+
+       		   for (Object element : list) {
+       			   if (element.startsWith("h"))
+       			     System.out.println(element);
+       		      }
+       	      }
+           /*
+            predicted error message: "cannot find symbol"
+            actual error message:
+              error: cannot find symbol
+			           if (element.startsWith("h"))
+			                      ^
+              symbol:   method startsWith(String)
+              location: variable element of type Object
+            */
+/* 4.  Workaround 0
+              A programmer should expect there to be a way to
+              work around the stumbling block, because
+              the JVM knows the type of an element.
+              Java's instanceof operator identifies the type
+              of an element to the JVM.
+            */
+  /* 5.  Stumbling block 1
+              However, use of the operator alone is insufficient,
+              because the compiler
+              objects to the following code that adds use of
+              the operator to the code from Stumbling block 0:
+            */
+            /* Code:
+
+            public class Test {
+
+  	         public static void main (String[] args) {
+
+  		         Object[] list =  new Object[1];
+
+  		         list[0] = new String("hi");
+
+  		         for (Object element : list) {
+
+  			          if (element.startsWith("h"))
+  			             System.out.println(element);
+  		          }
+  	           }
+             }
+             /*
+              predicted error message: "cannot find symbol"
+              actual error message:
+                error: cannot find symbol
+  			           if (element.startsWith("h"))
+  			                      ^
+                symbol:   method startsWith(String)
+                location: variable element of type Object
+              */
+
+/* 6. Workaround 1
+            Programmers use Java's casting facility
+            to tell the compiler
+            that code uses a subclass's method on an object,
+            even though the reference to the object is stored
+            in a super-class variable.
+            */
+
+/* 7. Stumbling block 2
+            However, use of this operator alone is insufficient,
+            because the compiler
+            objects to the following code that adds use of
+            the operator to the code from Stumbling block 0:
+            */
+            /* Code:
+            public class Test {
+
+	           public static void main (String[] args) {
+
+		           Object[] list =  new Object[2];
+
+		            list[0] = new String("hi");
+                list[1] = 1;
+
+		            for (Object element : list) {
+
+			             if (((String) element).startsWith("h"))
+			                System.out.println(element);
+		            }
+	             }
+             }
+             */
+             /*
+             predicted error message: cannot be cast
+
+             actual error message:
+             Exception in thread "main" java.lang.ClassCastException: java.base/java.lang.Integer cannot be cast to java.base/java.lang.String
+	             at Test.main(Test.java:12)
+
+        Use of that facility alone is insufficient because the JVM stumbles over not being able to cast certain subclasses of Object into other subclasses.
+        For example, it can't convert Integer to String, so a runtime error occurs.
+             */
+/* 8. Workaround 2
+            A programmer can combine use of both operators
+            to apply a method to exactly those
+            elements in the list that support the method.
+            */
+
+/*
+
+public class Test {
+
+	public static void main (String[] args) {
+
+		Object[] list =  new Object[2];
+
+		list[0] = new String("hi");
+    list[1] = 1;
+
+		for (Object element : list) {
+
+      if(element instanceof String) {
+			   if (((String) element).startsWith("h"))
+			      System.out.println(element);
+		  }
+	  }
+  }
+}
+*/
